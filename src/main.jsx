83002962e98d9e1489513dcc6599a38775c0a41c
Rasel Parvej <RaselParvejABC@gmail.com>
2022-11-26 20:24:01 +0600

    Navigation achieved after Adding Product

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import FirebaseAuthContextProvider from './contexts/FirebaseAuthContextProvider';
 import router from './router';
 import './main.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const queryClient = new QueryClient();
 
@@ -16,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <FirebaseAuthContextProvider>
         <RouterProvider router={router} />
+        <ToastContainer autoClose={5000} position="bottom-right" />
       </FirebaseAuthContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
